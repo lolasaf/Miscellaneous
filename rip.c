255532ea4bdf3a3b39c258e9b5f01a3748fd97aa
Lola <walaa.elsafadi@gmail.com>
2024-11-13 22:43:51 +0100

    :)

--- rip.c
+++ rip.c
@@ -17,23 +17,24 @@ For example this should work:
 
 (For readability reasons the '_' means space and the spaces are for readability only.)
 
-$> ./rip '( ()' | cat -e
-_ ()$
-(_ )$
+$> ./rip '( ( )' | cat -e
+_ ( ) $
+( _ ) $
 
-$> ./rip '( ( ( ) ( ) ) ( ) )' | cat -e
-( ( ( ) ( ) ) ( ) )$
+$> ./rip '( ( ( ) ( ) ( ) ) ( ) )' | cat -e
+( ( ( ) ( ) ( ) ) ( ) ) $
 
 $> ./rip '( ) ( ) ) ( )' | cat -e
 ( ) ( ) _ ( ) $
-( ) (_ ) _ ) $
-(_ ) ( ) _ ( ) $
+( ) ( _ ) ( ) $
+( _ ( ) ) ( ) $
 
-$> ./rip '( ( ) ( ( ) ( )' | cat -e
-( ( ) _ _ _ ) $
+$> ./rip '( ( ) ( ( ) (' | cat -e
 ( ( ) _ _ ) _ $
-( ( ) _ ) _ _ $
-(_ ( ) _ _ _ )
+( _ ) ( _ ) _ $
+( _ ) _ ( ) _ $
+_ ( ) ( _ ) _ $
+_ ( ) _ ( ) _ $
 */
 
 #include <stdio.h>
@@ -43,18 +44,34 @@ int	isbalance(char *str, int n)
 {
 	if (!str) return (1);
 	int counter = 0;
+	int removed = 0;
 	int i = 0;
 	while(str[i])
 	{
-		if ((str[i] == '(') && (1 & (n >> i)) ) 
-			counter++;
-		if ((str[i] == ')') && (1 & (n >> i)) )
-			counter--;
-		if (counter < 0) return (0);
+		if (str[i] == '(')
+		{
+			if (1 & (n >> i))
+				counter++;
+			else
+				removed++;
+		}
+		if (str[i] == ')')
+		{
+			if (1 & (n >> i))
+				counter--;
+			else
+				removed++;
+		}
+		else
+		{
+			if (!(1 & (n >> i)))
+				removed++;
+		}
+		if (counter < 0) return (-1);
 		i++;
 	}
-	if (counter) return (0);
-	return (1);
+	if (counter) return (-1);
+	return (removed);
 }
 
 int	ft_strlen(char *s)
@@ -74,14 +91,46 @@ int main(int ac, char **av)
 	int len = ft_strlen(av[1]);
 	int full_mask = (1 << len) - 1; // All bits set for the full string check
 	// first check if balanced as is
-	if (isbalance(av[1], full_mask))
+	if (isbalance(av[1], full_mask) > -1)
 	{
 		puts(av[1]);
 		return (0);
 	}
 	
-	int combs = 1 << len; // number of possible combinations
-	for (int i = 1; i < combs; i++)
+	// Get the lowest number to remove to balance
+	int removed = 0;
+	for (int i = full_mask; i > 0; i--)
+	{
+		removed = isbalance(av[1], i);
+		if (removed > -1)
+			break;
+	}
+	
+	for (int i = full_mask; i > 0; i--)
+	{
+		if (isbalance(av[1], i) == removed)
+		{
+			int j = 0;
+			while (av[1][j])
+			{
+				if (av[1][j] == '(' || av[1][j] == ')')
+				{
+					if ((1 & (i >> j)))
+					{
+						write(1, &av[1][j], 1);
+					}
+					else
+						write(1, " ", 1);
+				}
+				else
+					write(1, " ", 1);
+				j++;
+			}
+			write(1, "\n", 1);
+		}
+	}
+
+	/*for (int i = 1; i < combs; i++)
 	{
 		if (isbalance(av[1], i))
 		{
@@ -102,28 +151,8 @@ int main(int ac, char **av)
 					write(1, " ", 1);
 				j++;
 			}
-			puts("\n");
+			write(1, "\n", 1);
 		}
-	}
+	}*/
 	return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
